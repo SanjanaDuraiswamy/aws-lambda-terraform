44dbfd0445013b1357215bbe1371d876ca1f6770
Sanjana D <sanjana.d@brillio.com>
2026-05-11 14:29:49 +0530

    feat:add Lambda module main resource

---
+++ modules/lambda/main.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_function" "this" {
+  function_name    = var.function_name
+  role             = var.role_arn
+  handler          = var.handler
+  runtime          = var.runtime
+  filename         = var.filename
+  source_code_hash = filebase64sha256(var.filename)
+  timeout          = var.timeout
+  memory_size      = var.memory_size
+  tags             = var.tags
+}
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 14
+  tags              = var.tags
+}
