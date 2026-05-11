2d72b23227a55dbaf94dcb1cccaa889fec38607f
Sanjana D <sanjana.d@brillio.com>
2026-05-11 14:14:25 +0530

    feat:add root module calling lambda

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "lambda" {
+  source        = "./modules/lambda"
+  function_name = var.function_name
+  role_arn      = var.lambda_role_arn
+  filename      = "lambda.zip"
+  handler       = var.handler
+  runtime       = var.runtime
+  timeout       = var.timeout
+  memory_size   = var.memory_size
+  tags          = var.tags
+}
