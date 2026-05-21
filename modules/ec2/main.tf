ff0dd4f657148daf07dfd482fa083ab3fc2f1523
Sanjana D <sanjana.d@brillio.com>
2026-05-21 13:40:59 +0530

    fix: add permissions boundary to EC2 role

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 # EC2 role — this will be BLOCKED by boundary!
 resource "aws_iam_role" "ec2_exec" {
-  name = "${var.instance_name}-exec-role"
+  name                 = "${var.instance_name}-exec-role"
+  permissions_boundary = "arn:aws:iam::137982683320:policy/github-oidc-boundary"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
