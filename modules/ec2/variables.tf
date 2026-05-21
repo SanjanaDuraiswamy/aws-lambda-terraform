038de51e8bc949f3f5e0bfc2bfc138d2098232c7
Sanjana D <sanjana.d@brillio.com>
2026-05-21 14:14:18 +0530

    fix: correct Glue service role policy ARN

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -3,6 +3,18 @@ variable "instance_name" {
   type        = string
 }
 
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 us-east-1
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
 variable "tags" {
   description = "Mandatory tags"
   type        = map(string)
