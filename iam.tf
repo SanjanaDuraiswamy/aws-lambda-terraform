6e4459b491f8a39e139b6eba20329dc2944a11e2
Sanjana D <sanjana.d@brillio.com>
2026-05-21 12:27:30 +0530

    iam.tf

--- iam.tf
+++ iam.tf
@@ -1,3 +1,4 @@
+# Permissions Boundary Policy
 resource "aws_iam_policy" "oidc_boundary" {
   name        = "github-oidc-boundary"
   description = "Permissions boundary - Lambda, Glue, S3, CloudWatch"
@@ -19,4 +20,32 @@ resource "aws_iam_policy" "oidc_boundary" {
       }
     ]
   })
+}
+
+# Attach boundary as PERMISSIONS BOUNDARY to github-oidc-role
+resource "aws_iam_role" "github_oidc_role" {
+  name = "github-oidc-role"
+
+  permissions_boundary = aws_iam_policy.oidc_boundary.arn
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::137982683320:oidc-provider/token.actions.githubusercontent.com"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = "repo:SanjanaDuraiswamy/aws-lambda-terraform:*"
+        }
+      }
+    }]
+  })
+
+  tags = var.tags
 }
