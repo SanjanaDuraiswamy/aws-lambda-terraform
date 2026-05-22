9c84e0610bf42a2868e9e37aca593b66f05c4c5f
Sanjana D <sanjana.d@brillio.com>
2026-05-22 16:12:21 +0530

    Add iam:UpdateAssumeRolePolicy to inline policy

--- iam/boundary.tf
+++ iam/boundary.tf
@@ -17,7 +17,7 @@ resource "aws_iam_policy" "github_oidc_boundary" {
           "logs:*",
           "cloudwatch:*",
           "iam:*"
-        ]
+        ]  
         Resource = "*"
       }
     ]
